a0e1534f4245ffddfdce198b710fe89ddbfcd5d1
bibekpreet99 <43917624+bibekpreet99@users.noreply.github.com>
2018-10-28 12:57:31 +0530

    todo webapp

---
+++ script.js
@@ -0,0 +1,72 @@
+
+var ul = document.getElementById('list')
+var li;
+
+
+var addButton = document.getElementById('add')
+addButton.addEventListener('click', addItem)
+
+var addButton = document.getElementById('remove')
+addButton.addEventListener('click', removeItem)
+
+var deleteButton = document.getElementById('removeall')
+deleteButton.addEventListener('click', deleteList)
+
+
+
+
+
+
+
+function addItem(){
+    var input = document.getElementById('input');
+    var item = input.value;
+    var textnode = document.createTextNode(item)
+
+    if(item === ''){
+        return false;
+    }else{
+        // create li
+        li = document.createElement('li')
+
+        //create checkbox
+        var checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.setAttribute('id', 'check')
+
+        // create label
+        var label = document.createElement('label')
+       // label.setAttribute('for', 'item')
+
+        //add these elements on web page
+
+        ul.appendChild(label)
+        li.appendChild(checkbox)
+        label.appendChild(textnode)
+        li.appendChild(label)
+        ul.insertBefore(li, ul.childNodes[0])
+       
+        setTimeout(() => {
+            li.className = 'visual' 
+        }, 2);
+
+        input.value = ''
+    }
+}
+
+function removeItem(){
+    li = ul.children
+    for (let index = 0; index < li.length; index++) {
+        while (li[index] && li[index].children[0].checked) {
+            ul.removeChild(li[index])
+        }     
+    }
+}
+
+function deleteList(){
+    li = ul.children
+    for (let index = 0; index < li.length; index++){
+        console.log(li[index])
+        ul.removeChild(li[index])
+    }
+}
